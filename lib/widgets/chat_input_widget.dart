5d0e6948d6dffeef08d57caa9b1ab43a8a75376a
Don Lee <git@mrdonlee.addy.io>
2025-05-25 18:57:35 +0100

    feat: style chat input field

--- lib/widgets/chat_input_widget.dart
+++ lib/widgets/chat_input_widget.dart
@@ -10,16 +10,22 @@ class ChatInput extends StatefulWidget {
 class _ChatInput extends State<ChatInput> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: 600,
-      height: 300,
+      height: 110,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextField(
             decoration: InputDecoration(
-              border: OutlineInputBorder(),
+              border: InputBorder.none,
               hintText: 'How can I help you today?',
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
             ),
           ),
           Row(
